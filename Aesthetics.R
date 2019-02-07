935d718c1f0d0c8aff1c4fd28d3df292a25a05b3
Willamar <wfs20@hotmail.com>
2019-02-06 23:06:04 -0200

    '.'

--- Aesthetics.R
+++ Aesthetics.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # 1 - Map mpg to x and cyl to y
 ggplot(mtcars, aes(mpg, cyl)) +
   geom_point()
@@ -104,4 +106,75 @@ ggplot(mtcars, aes(mpg, qsec, col = factor(cyl), shape = factor(am))) +
 # Add mapping: (hp/wt) onto size
 ggplot(mtcars, aes(mpg, qsec, col = factor(cyl), shape = factor(am), size = (hp/wt))) +
   geom_point()
+
+
+cyl.am <- ggplot(mtcars, aes(x = factor(cyl), fill = factor(am)))
+
+
+# The base layer, cyl.am, is available for you
+# Add geom (position = "stack" by default)
+cyl.am + 
+  geom_bar(position = 'stack')
+
+# Fill - show proportion
+cyl.am + 
+  geom_bar(position = 'fill')  
+
+# Dodging - principles of similarity and proximity
+cyl.am +
+  geom_bar(position = 'dodge') 
+
+# Clean up the axes with scale_ functions
+val = c("#E41A1C", "#377EB8")
+lab = c("Manual", "Automatic")
+cyl.am +
+  geom_bar(position = "dodge") +
+  scale_x_discrete("Cylinders") + 
+  scale_y_continuous("Number") +
+  scale_fill_manual("Transmission", 
+                    values = val,
+                    labels = lab) 
+
+# 1 - Create jittered plot of mtcars, mpg onto x, 0 onto y
+ggplot(mtcars, aes(x = mpg, y = 0)) +
+  geom_jitter()
+
+# 2 - Add function to change y axis limits
+ggplot(mtcars, aes(x = mpg, y = 0)) +
+  geom_jitter() + 
+  scale_y_continuous(limits = c(-2,2))
+
+
+# Basic scatter plot: wt on x-axis and mpg on y-axis; map cyl to col
+ggplot(mtcars, aes(x=wt, y = mpg, color = cyl)) + 
+  geom_point(size = 4)
+
+# Hollow circles - an improvement
+ggplot(mtcars, aes(x=wt, y = mpg, color = cyl)) + 
+  geom_point(size = 4, shape = 1)
+
+
+# Add transparency - very nice
+ggplot(mtcars, aes(x=wt, y = mpg, color = cyl)) + 
+  geom_point(size = 4, shape = 1, alpha = 0.6)
+
+
+# Scatter plot: carat (x), price (y), clarity (color)
+ggplot(diamonds, aes(x = carat, y = price, color = clarity)) +
+  geom_point()
+
+
+# Adjust for overplotting
+ggplot(diamonds, aes(x = carat, y = price, color = clarity)) +
+  geom_point(alpha = 0.5)
+
+
+# Scatter plot: clarity (x), carat (y), price (color)
+ggplot(diamonds, aes(x = clarity, y = carat, color = price)) + 
+  geom_point(alpha = 0.5)
+
+
+# Dot plot with jittering
+ggplot(diamonds, aes(x = clarity, y = carat, color = price)) + 
+  geom_point(alpha = 0.5, position = 'jitter')
 
